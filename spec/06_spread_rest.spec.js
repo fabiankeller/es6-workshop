a4d08012285beb838e289c749e8b047b06084e6e
Fabian Keller (taakefa2) <fabian.keller@swisscom.com>
2016-11-24 13:46:21 +0100

    removed test with prototypes

--- spec/06_spread_rest.spec.js
+++ spec/06_spread_rest.spec.js
@@ -29,27 +29,4 @@ describe('Rest Parameters', () => {
     expect(resty(1,2,3)).toBe(/*ENTER YOUR GUESS HERE*/);
     expect(resty(1,2,3, undefined, 5, undefined, 7, undefined, 9, 10)).toBe(/*ENTER YOUR GUESS HERE*/);
   });
-
-  xit('is an actual array, unlike arguments', () => {
-
-    function resty(...args){
-      return args;
-    }
-
-    function argy(){
-      return arguments;
-    }
-
-    var args = argy(1,2,3);
-    var rests = resty(1,2,3);
-
-    //
-    expect(args.__proto__ == rests.__proto__) .toBe(/*ENTER YOUR GUESS HERE*/);
-    expect(args.splice)                       .toBe(/*ENTER YOUR GUESS HERE*/);
-
-    expect(rests.__proto__)                   .toEqual(/*ENTER YOUR GUESS HERE*/);
-    expect(rests.splice)                      .toBeDefined();
-    expect(rests.splice)                      .toEqual(Array.prototype.splice);
-
-  });
 });
